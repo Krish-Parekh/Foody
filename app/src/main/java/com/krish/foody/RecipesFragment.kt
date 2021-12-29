2333104131db6523446ca404c8b4db734012a141
Krish Parekh <Krishp4123@gmail.com>
2021-12-29 21:23:56 +0530

    Foody - Design Recipe Fragment with shimmer layout

--- app/src/main/java/com/krish/foody/RecipesFragment.kt
+++ app/src/main/java/com/krish/foody/RecipesFragment.kt
@@ -1,21 +1,22 @@
 package com.krish.foody
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.krish.foody.databinding.FragmentRecipesBinding
 
 
 class RecipesFragment : Fragment() {
-
-
+    private lateinit var binding: FragmentRecipesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
